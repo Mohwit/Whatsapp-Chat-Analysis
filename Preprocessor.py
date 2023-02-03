22ce4c838c92d016104b2d890bdbd240c625f84f
Mohwit <mohwits@gmail.com>
2023-02-03 22:20:39 +0530

    Update Preprocessor file

--- Preprocessor.py
+++ Preprocessor.py
@@ -50,8 +50,22 @@ def preprocess(data):
     ## and creating specific column
     df['Year'] = df['Date'].dt.year
     df['Month'] = df['Date'].dt.month_name()
+    df['Month_Number'] = df['Date'].dt.month
     df['Day'] = df['Date'].dt.day
     df['Hour'] = df['Date'].dt.hour
     df['Minute'] = df['Date'].dt.minute
+    df['Only_Date'] = df['Date'].dt.date
+    df['Day_Name'] = df['Date'].dt.day_name
+
+    period = []
+    for hour in df[['Day_Name', 'Hour']]['Hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+
+    df['Period'] = period
 
     return df
